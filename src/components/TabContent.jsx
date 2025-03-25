cd5d30cd3bc47a30f8d8ab15f776aa34c216991d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-25 17:13:36 +0100

    add renderContent function and error message

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -1,13 +1,17 @@
 export default function TabContent({ data, openFlag }) {
 
+    function renderContent() {
+        return data.map(item => (
+            <div>
+                <h3>{openFlag === item.id ? item.title : ''}</h3>
+                <p>{openFlag === item.id ? item.content : ''}</p>
+            </div>
+        ))
+    }
+
     return (
         <div className="tab_content">
-            {data.map(item => (
-                <div>
-                    <h3>{openFlag === item.id ? item.title : ''}</h3>
-                    <p>{openFlag === item.id ? item.content : ''}</p>
-                </div>
-            ))}
+            {(openFlag === 0 ? 'Seleziona un Tab' : renderContent())}
         </div>
     )
 }
