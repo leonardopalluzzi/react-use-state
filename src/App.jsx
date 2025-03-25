0833789f0e82114e0968715115d39c1e46aaf643
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-25 14:37:18 +0100

    add page base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
+      <header>
+        <h1 className='title'>Learn Web Development</h1>
+      </header>
 
+      <Main />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div className="container">
+            <h2>main section</h2>
+        </div>
+
+    )
+}
